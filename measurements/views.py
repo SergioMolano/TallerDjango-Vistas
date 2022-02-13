69d6e8a4e010c993b1aab6c7474761c53f715cea
Sergio Molano <sa.molano@uniandes.edu.co>
2022-02-12 21:11:41 -0500

    Avance

--- measurements/views.py
+++ measurements/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def measurements_view(request):
+    if request.method == 'GET':
+        measurements = vl.get_variables()
+        measurements_dto = serializers.serialize('json', variables)
+        return HttpResponse(measurements_dto, 'application/json')
+
+def measurement_view(request, pk):
+    if request.method == 'GET':
+        measurement = vl.get_measurement(pk)
+        measurement_dto = serializers.serialize('json', measurement)
+        return HttpResponse(measurement_dto, 'application/json')
